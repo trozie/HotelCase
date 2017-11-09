902d64d8ef830dfe3e94c8825315eacdb1053e85
trozie <twanrozie@gmail.com>
2017-11-09 11:11:45 +0100

    Add project files.

---
+++ HotelCase/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HotelCase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
